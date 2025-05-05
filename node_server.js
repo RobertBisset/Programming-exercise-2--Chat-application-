4170bb3c2ca9e35b6e42b7466c8a12278648e92b
RobertBisset <u13rb24@abdn.ac.uk>
2025-05-05 18:54:01 +0100

    added container to show all online users

--- node_server.js
+++ node_server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const socket=require("socket.io");
+const user_list=[]
 
 app.use(express.static("static"));
 
@@ -11,10 +12,23 @@ const io=socket(server)
 
 io.on("connection", (socket)=>{
     console.log("user connected")
+    io.emit("message", {name:"Server", message:"user connected"});
+    socket.on("name", (data)=>{
+        socket.user=data.name
+        if (!((user_list).includes(data.name))){
+            user_list.push(data.name)
+            io.emit("user",user_list);
+        }
+    })
     socket.on("message",(data)=>{
         io.emit("message", data);
     });
     socket.on("disconnect",()=>{
+        console.log(socket.user)
         console.log("user disconnected");
+        io.emit("message", {name:"Server", message:"user disconnected"});
+        if (user_list.indexOf(socket.user)!=-1){
+        user_list.splice(user_list.indexOf(socket.user), 1)}
+        io.emit("user",user_list);
     });
 });
--- static/chat_room.js
+++ static/chat_room.js
@@ -4,12 +4,26 @@ const form=document.getElementById("form");
 const name=document.getElementById("name");
 const message=document.getElementById("message");
 const chatroom=document.getElementById("chatroom");
+const user_list_box=document.getElementById("user_list_box")
+const form_name=document.getElementById("form_name");
+let username=""
 
-form.addEventListener("submit", function(e) {
+Array.from(form.elements).forEach(formElement => formElement.disabled = true);
+form.addEventListener("submit", function(e){
     e.preventDefault();
     Socket.emit("message", {
-        name:name.value,
-        message:message.value
+        message:message.value,
+        name:username
+    });
+});
+
+form_name.addEventListener("submit", function(e){
+    e.preventDefault();
+    Array.from(form.elements).forEach(formElement => formElement.disabled = false);
+    Array.from(form_name.elements).forEach(formElement => formElement.disabled = true);
+    username=name.value
+    Socket.emit("name",{
+        name:username
     });
 });
 
@@ -20,3 +34,12 @@ Socket.on("message", (data)=>{
     message.value=""
     window.scrollTo(0, document.body.scrollHeight);
 })
+
+Socket.on("user", (data2)=>{
+    user_list_box.innerHTML=""
+    for (var i=0; i<(data2.length);i++){
+        const user_area=document.createElement("p");
+        user_area.textContent=data2[i]
+        user_list_box.appendChild(user_area)
+    };
+});
